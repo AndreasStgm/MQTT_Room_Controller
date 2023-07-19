94c470fcf3957f127c620d685910314ea07b5a51
AndreasStgm <andreassteegmans@hotmail.com>
2023-07-19 23:51:30 +0200

    MQTT configuration checks
    Added simple prompts to notify the user that certain inputs don't have the necessary values

--- RoomControllerApp/MqttConfig.xaml.cs
+++ RoomControllerApp/MqttConfig.xaml.cs
@@ -11,26 +11,48 @@ public partial class MqttConfig : ContentPage
     public MqttConfig()
     {
         InitializeComponent();
+
+        ToggleCredentialsVisibility(false);
     }
 
     private void CredentialsSwitch_Toggled(object sender, ToggledEventArgs e)
     {
         if (CredentialsSwitch.IsToggled)
         {
-            UsernameLine.IsVisible = true;
-            PasswordLine.IsVisible = true;
+            ToggleCredentialsVisibility(true);
         }
         else
         {
-            UsernameLine.IsVisible = false;
-            PasswordLine.IsVisible = false;
+            ToggleCredentialsVisibility(false);
         }
     }
 
     private void SaveButton_Clicked(object sender, EventArgs e)
     {
-        //Display a confirmation message to make it more user-friendly
-        Toast.Make("Configuration Saved", ToastDuration.Short, 14).Show();
+        if (IpEntry.Text.Trim() == string.Empty)
+        {
+            IpEntry.Focus();
+            DisplayAlert("Insuffcient Information", "Broker IP needs a value.", "OK");
+        }
+        else if (PortEntry.Text.Trim() == string.Empty)
+        {
+            DisplayAlert("Insuffcient Information", "Broker port needs a value.", "OK");
+        }
+        else
+        {
+            //Display a confirmation message to make it more user-friendly
+            Toast.Make("Configuration Saved", ToastDuration.Short, 14).Show();
+        }
+    }
+
+    private void ToggleCredentialsVisibility(bool state)
+    {
+        UsernameLine.IsVisible = state;
+        PasswordLine.IsVisible = state;
+
+        //Clear the entry fields
+        UsernameEntry.Text = string.Empty;
+        PasswordEntry.Text = string.Empty;
     }
 
     private async void CreateClient()
